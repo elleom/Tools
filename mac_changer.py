165905a9b9f11607560242d4d5f141214464442b
0elle0 <el_leo_m@hotmail.com>
2020-11-21 19:38:54 -0500

    add user input parser

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,21 @@
-import subprocess
+#!/usr/bin/env python
 
-iface = "wlan0"
-ether = "00:11:22:33:44:55"
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--iface", dest="interface", help="Interface to change MAC")
+parser.add_option("-m", "--mac", dest="new_mac", help="new MAC")
+
+(options, arguments) = parser.parse_args()
+
+iface = options.interface
+new_mac = options.new_mac
 
 print('[*] Changing MAC Address for' + iface)
-print('[*] MAC Addrress set to' + ether)
+print('[*] MAC Addrress set to' + new_mac)
 
-subprocess.call(["ifconfig", iface, "down"], shell=True) # input hygiene
-subprocess.call(["ifconfig", iface, "hw", ether],  shell=True)
-subprocess.call(["ifconfig", iface, "up"], shell=True)
+subprocess.call(["ifconfig", iface, "down"]) # input hygiene
+subprocess.call(["ifconfig", iface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", iface, "up"])
