bf5160afa2958464018da27c2e1474b6291f1e5c
elleom <el_leo_m@hotmail.com>
2021-02-10 15:45:45 -0500

    -- queue

--- net_cut/net_cut.py
+++ net_cut/net_cut.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
 # sudo iptables -I FORWARD --jump NFQUEUE --queue-num 0
-# requires netfilterqueue
+# requires netfilterqueue , not available for python 3.7+
 
+import netfilterqueue
+
+
+def process_packet(packet):
+    print(packet)
+
+
+queue = netfilterqueue.Netfilterqueue
+queue.bind(0, process_packet)
+queue.run()
