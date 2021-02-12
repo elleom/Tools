eebada50c1cf1b3ac2e3b700f8f39c680b71e83f
elleom <el_leo_m@hotmail.com>
2021-02-12 16:35:02 -0500

    netfilterqueue dependencies OK --see comms

--- net_cut/net_cut.py
+++ net_cut/net_cut.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 # sudo iptables -I FORWARD --jump NFQUEUE --queue-num 0
-# requires netfilterqueue , not available for python 3.7+
+# requires netfilterqueue , not available for python 3.7+ via pip,
+# install from source
+# https://github.com/kti/python-netfilterqueue
+
+# git clone git@github.com:kti/python-netfilterqueue.git
+# cd python-netfilterqueue
+# python setup.py install
+
 
 import netfilterqueue
 
