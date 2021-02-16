b25cb6eb877946e528cfa5cca5182a2a433e2a75
elleom <el_leo_m@hotmail.com>
2021-02-16 12:40:17 -0500

    fix typo

--- net_cut/net_cut.py
+++ net_cut/net_cut.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 # sudo iptables -I FORWARD --jump NFQUEUE --queue-num 0
+# sudo iptables -I INPUT --jump NFQUEUE --queue-num 0
+# sudo iptables -I OUTPUT --jump NFQUEUE --queue-num 0
 # requires netfilterqueue , not available for python 3.7+ via pip,
 # install from source
 # https://github.com/kti/python-netfilterqueue
@@ -14,8 +16,9 @@ import netfilterqueue
 
 def process_packet(packet):
     print(packet)
+    packet.accept()
 
 
-queue = netfilterqueue.Netfilterqueue()
+queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)  # 0 STANDS FOR THE QUEUE NUMBER
 queue.run()
