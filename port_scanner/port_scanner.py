36ad7b3fe8fc75ebc7aa3b06ac936e5f680504d7
oel_leo <el_leo_m@hotmail.com>
2023-03-26 09:24:57 -0400

    add search resolve url. Closes #4

--- port_scanner/port_scanner.py
+++ port_scanner/port_scanner.py
@@ -2,25 +2,29 @@
 
 import socket
 import argparse
-from IPy import IP
+import header.header_banner
 
 
-def scan(ip_address, port):
-    print(ip_address, port)
+def scan(ip_address, port, url=None):
     try:
+        if url:
+            ip_address = socket.gethostbyname(url)
+            print(f'[!] {url} resolved to {ip_address}')
         sock = socket.socket()
         sock.connect((ip_address, port))
         print(f'[*] Port {port} is open')
     except ConnectionError:
         print(f'[*] Port {port} seems to be closed or filtered')
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--ip', dest='target_ip', help="IP to target interface")
     parser.add_argument('--port', dest='port', help="port to scan")
+    parser.add_argument('--url', dest='url', help="Target's URL")
     options = parser.parse_args()
-    if not options.target_ip:
-        parser.error("[!] Introduce target's IPV4")
+    if not options.target_ip and not options.url:
+        parser.error("[!] Introduce target's IPV4 or URL")
     elif not options.port:
         parser.error("[!] Introduce port to scan")
     return options
@@ -28,7 +32,11 @@ def get_arguments():
 
 if '__main__':
     try:
+        header.header_banner.print_header()
+    except ModuleNotFoundError:
+        print('[!] MuiausOnTheWire :) is launching...')
+    try:
         args = get_arguments()
-        scan(args.target_ip, int(args.port))
+        scan(args.target_ip, int(args.port), args.url)
     except KeyboardInterrupt:
         print('[-] Program terminated')
