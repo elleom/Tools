edee475cd315d069a0a1c89f8e5ea71e964a2c28
oel_leo <el_leo_m@hotmail.com>
2023-05-11 15:18:33 -0400

    add scanner skel

---
+++ vuln_scanner/vuln_scanner.py
@@ -0,0 +1,12 @@
+import port_scanner.port_scanner
+
+target_ip = input("introduce target")
+port_number = input("integer")
+vuln_file = input('path to file')
+
+def init():
+    pass
+
+
+if __name__ == "__main__":
+    init()
