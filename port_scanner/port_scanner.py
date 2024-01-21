90c1190e799766328c44da45c5b0f0af8bf46c2f
oel_leo <el_leo_m@hotmail.com>
2024-01-21 15:16:00 -0500

    add vuln scanner and refactor port scanner to class

--- port_scanner/port_scanner.py
+++ port_scanner/port_scanner.py
@@ -7,76 +7,81 @@ from colorama import Fore, Style
 from IPy import IP
 
 
-def get_ports(ports):
-    ports_array = []
-    if '/' in ports:
-        # handles range
-        ports_array = ports.split('/')
-        return ports_array, True
-    elif '-' in ports:
-        ports_array = ports.split('-')
-        return ports_array, False
-    else:
-        ports_array.append(ports)
-        return ports_array, False
+class PortScan:
 
+    def __init__(self, target, ports):
+        self.target = target
+        self.ports = ports
 
-def get_banner(sock):
-    return sock.recv(1024)
+    def get_ports(self):
+        ports_array = []
+        if '/' in self.ports:
+            # handles range
+            ports_array = self.ports.split('/')
+            return ports_array, True
+        elif '-' in self.ports:
+            ports_array = self.ports.split('-')
+            return ports_array, False
+        else:
+            ports_array.append(self.ports)
+            return ports_array, False
 
+    @staticmethod
+    def get_banner(sock):
+        return sock.recv(1024)
 
-def get_port_status(ip_address, port):
+    @staticmethod
+    def scan_port(ip_address, port):
 
-    try:
-        sock = socket.socket()
-        sock.settimeout(0.5)
-        sock.connect((ip_address, port))
-        print(f' ... port {port} is ' + Fore.GREEN + 'OPEN' + Style.RESET_ALL)
         try:
-            banner = get_banner(sock)
-            print(f'[+] Open port {str(port)}: \n {str(banner)}')
-        except:
-            print(f'[+] Open port {str(port)} \'s banner is hidden')
-        sock.close()
-    except TimeoutError or ConnectionError:
-        print(f' ... port {port} seems to be'
-              , Fore.RED + 'CLOSED'
-              , Fore.LIGHTWHITE_EX + 'or'
-              , Fore.YELLOW + 'FILTERED'
-              + Style.RESET_ALL, sep=' ')
+            sock = socket.socket()
+            sock.settimeout(0.5)
+            sock.connect((ip_address, port))
+            print(f' ... port {port} is ' + Fore.GREEN + 'OPEN' + Style.RESET_ALL)
+            try:
+                banner = PortScan.get_banner(sock)
+                print(f'[+] Open port {str(port)}: \n {str(banner)}')
+            except Exception as e:
+                print(e)
+                print(f'[+] Open port {str(port)} \'s banner is hidden')
+            sock.close()
+        except TimeoutError or ConnectionError:
+            print(f' ... port {port} seems to be'
+                  , Fore.RED + 'CLOSED'
+                  , Fore.LIGHTWHITE_EX + 'or'
+                  , Fore.YELLOW + 'FILTERED'
+                  + Style.RESET_ALL, sep=' ')
 
-
-def check_ip(target):
-    try:
-        IP(target)
-        return target
-    except ValueError:
-        target_ip = socket.gethostbyname(target)
-        print(f'[!] {target} resolved to {target_ip}')
-        return target_ip
-
-
-def parse_scan_vars(targets, ports):
-
-    (ports, is_range) = get_ports(ports)
-
-    for target in targets.split(','):
-        print(f'[*] Initiating scan on {target}')
-        ip_addr = check_ip(target)
+    def check_ip(self):
         try:
-            if is_range:
-                print(f'[*] Scanning range {ports[0]}:{ports[1]}')
-                for port in range(int(ports[0]), int(ports[1])+1):
-                    get_port_status(ip_addr, port)
-            else:
-                for port in ports:
-                    print(f'[*] Scanning port {port}', end='')
-                    get_port_status(ip_addr, int(port))
-        except ConnectionError:
-            if port == [ports[-1]]:
-                continue
-            else:
-                print(f'[*] Port {port} seems to be closed or filtered')
+            IP(self.target)
+            return self.target
+        except ValueError:
+            target_ip = socket.gethostbyname(self.target)
+            print(f'[!] {self.target} resolved to {target_ip}')
+            return target_ip
+
+    def parse_scan_vars(self, targets):
+
+        (ports, is_range) = PortScan.get_ports()
+
+        for target in targets.split(','):
+            print(f'[*] Initiating scan on {target}')
+            ip_addr = PortScan.check_ip(target)
+            try:
+                if is_range:
+                    print(f'[*] Scanning range {ports[0]}:{ports[1]}')
+                    for port in range(int(ports[0]), int(ports[1]) + 1):
+                        PortScan.scan_port(port)
+                else:
+                    for port in ports:
+                        print(f'[*] Scanning port {port}', end='')
+                        PortScan.scan_port(int(port))
+            except ConnectionError:
+                if port == [ports[-1]]:
+                    continue
+                else:
+                    print(f'[*] Port {port} seems to be closed or filtered')
 
 
 def get_arguments():
@@ -99,6 +104,6 @@ if __name__ == '__main__':
         print('[!] MuiausOnTheWire :) is launching...')
     try:
         args = get_arguments()
-        parse_scan_vars(args.target_ip, args.port)
+        PortScan.parse_scan_vars(args.target_ip, args.port)
     except KeyboardInterrupt:
         print('[-] Program terminated')
--- vuln_scanner/vuln_scanner.py
+++ vuln_scanner/vuln_scanner.py
@@ -1,12 +1,26 @@
-import port_scanner.port_scanner
-
-target_ip = input("introduce target")
-port_number = input("integer")
-vuln_file = input('path to file')
-
-def init():
-    pass
+from port_scanner.port_scanner import PortScan
+import argparse
 
 
-if __name__ == "__main__":
-    init()
+def get_arguments() -> argparse.Namespace:
+    """
+    :return: arguments dict
+    """
+    parser = argparse.ArgumentParser(prog="Vulnerability Scanner")
+    parser.add_argument("-port", dest="port", action="store", required=True, type=int)
+    parser.add_argument("-target", dest="target", action="store", required=True, type=str)
+
+    return parser.parse_args()
+
+
+def init(target, port):
+    target_ip = target
+    port_number = port
+    ps = PortScan
+    ps.scan_port(target_ip, int(port_number))
+
+
+if __name__ == '__main__':
+    args = get_arguments()
+    init(args.target, args.port)
+
